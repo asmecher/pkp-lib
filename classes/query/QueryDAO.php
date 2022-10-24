682a4de7ecbe78c07b867bb5b2d170cc4aadd9a0
Nate Wright <natew@notthisway.com>
2022-10-24 13:43:55 +0100

    pkp/pkp-lib#7191 Changes after code review
    - Localize category breadcrumb
    - Add |escape to smarty template variables
    - Fix foreign key and index errors in editor assignment migration
    - Remove duplicate index on subeditor_submission_group
    - Fix user group migration for subeditor assignments
    - Fix submit as user
    - Fix citations in wizard review step
    - Add missing escaping in template files
    - Allow null parameter in DataObjectRequiredPolicy
    - Refactor contributor form to remove empty classes

--- classes/query/QueryDAO.php
+++ classes/query/QueryDAO.php
@@ -112,7 +112,7 @@ class QueryDAO extends \PKP\db\DAO
     }
 
     /**
-     * Retrieve a count of all queries totalled by stage
+     * Retrieve a count of all open queries totalled by stage
      *
      * @param int[] $participantIds Only include queries with these participants
      *
